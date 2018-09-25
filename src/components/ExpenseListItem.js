420e7f14f648bb78c9681cd9ce4ff33d5ea50c4e
318Thorne <c3186564@uon.edu.au>
2018-09-25 15:45:55 +1000

    set up numerical formatting for currenct

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import moment from 'moment';
+import numeral from 'numeral'
 
 export const ExpenseListItem = ({id , description, amount, createdAt}) => (
     <div>
-        <p>{description}</p>
-        <p>{amount}</p>
-        <p>{createdAt}</p>
-        <Link to={`/edit/${id}`}>edit</Link>
-        <hr/>
+        <Link to={`/edit/${id}`}>
+            <h3>{description}</h3>
+        </Link>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')}
+            -
+            {moment(createdAt).format('DD/MM/YY')}
+        </p>
     </div>
 );
 
